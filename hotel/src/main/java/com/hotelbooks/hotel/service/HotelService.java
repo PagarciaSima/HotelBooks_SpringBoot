3396ac6973938360fc6089f63276d15774da810f
pagarciaSim <pablo.garciasimavilla@gmail.com>
2025-01-16 21:44:07 +0100

    Updated docker compose & image management

--- hotel/src/main/java/com/hotelbooks/hotel/service/HotelService.java
+++ hotel/src/main/java/com/hotelbooks/hotel/service/HotelService.java
@@ -7,6 +7,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.hotelbooks.hotel.dto.HotelAvailableDTO;
@@ -16,48 +18,54 @@ import com.hotelbooks.hotel.dto.ServiceDTO;
 import com.hotelbooks.hotel.entity.HotelAvailable;
 import com.hotelbooks.hotel.repository.HotelRepository;
 
-import lombok.AllArgsConstructor;
-
 @Service
-@AllArgsConstructor
 public class HotelService {
 
-	private HotelRepository hotelRepository;
-	private HotelServiceClient hotelServiceClient;
-	private HotelReviewClient hotelReviewClient;
-	
-	public List<HotelAvailableDTO> getAvailableHotelsWithServicesAndReviews(
-		Date startDate, Date endDate, Integer cityId
-	) {
-		
-		// STEP 1: find available hotels
-		List<HotelAvailable> availableHotels = hotelRepository.findAllAvailableHotelsByCity(startDate, endDate, cityId);
-		
-		if (availableHotels.isEmpty()) {
-			return new ArrayList<>();
-		} else {
-			// STEP 2: Hotel Ids
-			List<Long> hotelIds = availableHotels.stream().map(HotelAvailable :: getId).collect(Collectors.toList());
-			// STEP 3: Consume services micro service
-			List<HotelServiceDTO> hotelServices = hotelServiceClient.getHotelServices(hotelIds);
-			// STEP 4: Consume reviews micro service
-			List<HotelReviewDTO> hotelReviews = hotelReviewClient.getHotelReviews(hotelIds);
-			// STEP 5: Combine results
-			Map<Long, List<ServiceDTO>> servicesByHotelId = hotelServices.stream()
-					.collect(Collectors.toMap(HotelServiceDTO :: getHotelId, HotelServiceDTO :: getServices));
+    @Autowired
+    private HotelRepository hotelRepository;
 
-			Map<Long, Double> reviewsByHotelId = hotelReviews.stream()
-					.collect(Collectors.toMap(HotelReviewDTO :: getHotelId, HotelReviewDTO :: getAverageCalification));
-			
-			// STEP 6: Group and map results
-			return availableHotels.stream()
-					.map(hotel -> {
-			            HotelAvailableDTO dto = new HotelAvailableDTO(hotel);
-			            dto.setServices(servicesByHotelId.getOrDefault(hotel.getId(), Collections.emptyList()));
-			            dto.setAverageCalification(reviewsByHotelId.getOrDefault(hotel.getId(), null));
-			            return dto;
-					}).collect(Collectors.toList());
-			
-		}
-	}
+    @Autowired
+    private HotelServiceClient hotelServiceClient;
+
+    @Autowired
+    private HotelReviewClient hotelReviewClient;
+    
+    @Value("${server.url}")
+    private String serverUrl;
+
+    public List<HotelAvailableDTO> getAvailableHotelsWithServicesAndReviews(Date startDate, Date endDate, Integer cityId) {
+        // Paso 1: Obtener hoteles disponibles
+        List<HotelAvailable> availableHotels = hotelRepository.findAllAvailableHotelsByCity(startDate, endDate, cityId);
+
+        if  (availableHotels.isEmpty()) {
+        	List<HotelAvailableDTO> dto = new ArrayList<>();
+        	return new ArrayList<>(dto);
+        } else {
+	        // Paso 2: Obtener los IDs de los hoteles
+	        List<Long> hotelIds = availableHotels.stream().map(HotelAvailable::getId).collect(Collectors.toList());
+	
+	        // Paso 3: Consumir el microservicio de servicios de hotel
+	        List<HotelServiceDTO> hotelServices = hotelServiceClient.getHotelServices(hotelIds);
+	
+	        // Paso 4: Consumir el microservicio de reviews de hotel
+	        List<HotelReviewDTO> hotelReviews = hotelReviewClient.getHotelReviews(hotelIds);
+	
+	        // Paso 5: Combinar los resultados de servicios y reviews
+	        Map<Long, List<ServiceDTO>> servicesByHotelId = hotelServices.stream()
+	                .collect(Collectors.toMap(HotelServiceDTO::getHotelId, HotelServiceDTO::getServices));
+	
+	        Map<Long, Double> reviewsByHotelId = hotelReviews.stream()
+	                .collect(Collectors.toMap(HotelReviewDTO::getHotelId, HotelReviewDTO::getAverageCalification));
+	
+	        // Paso 6: Agrupar y mapear los resultados
+	        return availableHotels.stream().map(hotel -> {
+	        	String imageUrl = serverUrl + "/images/" + hotel.getPicture(); 
+	        	hotel.setPicture(imageUrl);
+	            HotelAvailableDTO dto = new HotelAvailableDTO(hotel);
+	            dto.setServices(servicesByHotelId.getOrDefault(hotel.getId(), Collections.emptyList()));
+	            dto.setAverageCalification(reviewsByHotelId.getOrDefault(hotel.getId(), null));
+	            return dto;
+	        }).collect(Collectors.toList());
+        }
+    }
 }
